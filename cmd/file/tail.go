62c6dae5ef9d4e61eb03014605523ea305f8d027
mas2020 <andrea.genovesi@gmail.com>
2024-02-16 18:25:56 +0100

    #13 refactor the tail command, fixing a bug from issue #60

--- cmd/file/tail.go
+++ cmd/file/tail.go
@@ -5,11 +5,9 @@ package file
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/mas2020-golang/goutils/output"
-	"github.com/mas2020-golang/ion/packages/utils"
+	"github.com/mas2020-golang/ion/packages/file"
 	"github.com/spf13/cobra"
 )
 
@@ -33,7 +31,8 @@ $ cat test.txt | ion tail --rows 10`,
 The command can read the standard input or a given file and returns the corresponding rows.
 If the --rows is not given, the command returns the last 10 rows.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			lines, err := tail(args, rows)
+			t := file.NewTail()
+			lines, err := t.Tail(args, rows)
 			output.CheckErrorAndExit("", "", err)
 			for _, l := range lines {
 				fmt.Print(l)
@@ -46,78 +45,4 @@ If the --rows is not given, the command returns the last 10 rows.`,
 	//err := cmd.MarkFlagRequired("rows")
 	//utils.Check(err)
 	return cmd
-}
-
-func tail(args []string, r int) (lines []string, err error) {
-	var (
-		f *os.File = utils.GetBytesFromPipe()
-	)
-	if f == nil {
-		if len(args) == 0 {
-			output.CheckErrorAndExit("", "", fmt.Errorf("no file argument"))
-		}
-		// load the file into the buffer
-		f, err = os.Open(args[0])
-		output.CheckErrorAndExit("", "", err)
-	}
-	return getLines(f, r)
-}
-
-// getLines returns the n lines in buf
-func getLines(f *os.File, n int) (lines []string, err error) {
-	var (
-		b      []byte
-		l      [][]byte
-		pos, i int
-	)
-
-	defer func() {
-		err := f.Close()
-		output.CheckErrorAndExit("", "", err)
-	}()
-
-	buf, err := ioutil.ReadAll(f)
-	//fmt.Printf("file content (bytes): %v\n", buf)
-	output.CheckErrorAndExit("", "", err)
-	pos = len(buf) - 1
-
-	for i < n && pos >= 0 {
-		// \n
-		if buf[pos] == 10 {
-			if len(b) == 0 {
-				b = append(b, buf[pos])
-				pos--
-			} else {
-				// create a new line
-				l = append(l, reverseSlice(b))
-				b = make([]byte, 0)
-				i++
-			}
-		} else {
-			b = append(b, buf[pos])
-			pos--
-		}
-		// edge case: beginning of the file
-		if pos == -1 && len(b) > 0 {
-			l = append(l, reverseSlice(b))
-		}
-	}
-	// create the slice of lines
-	for i := len(l) - 1; i >= 0; i-- {
-		lines = append(lines, string(l[i]))
-	}
-	return
-}
-
-func reverseSlice(b []byte) []byte {
-	var (
-		r []byte
-	)
-	if len(b) == 1 {
-		return b
-	}
-	for i := len(b) - 1; i >= 0; i-- {
-		r = append(r, b[i])
-	}
-	return r
 }
